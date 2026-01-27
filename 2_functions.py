dc476c2c916f551f49ea7b93bde479cc22103811
asanchez365365 <saintxotic123@gmail.com>
2026-01-27 14:49:51 +0000

    functions

--- 2_functions.py
+++ 2_functions.py
@@ -5,7 +5,8 @@
 
 
 
-
+def greet():
+    print("Hello World")
 
 
 
@@ -13,17 +14,24 @@
 # Declare a Function: Create a function called welcome that takes a person's name as an argument. This function should print "Welcome {name}!" whenever it is called.
 
 # Create a Variable: Define a variable named name and store any name of your choice in it.
-
+name = "Alice"
 # Note: Only define the function and create the variable. Do not call the function afterwards.
 
 
-
+def welcome(name):
+    print(f"welcome{ name }!")
 
 
 
 # Create Functions Practice #3
-# Declare a function called square, that takes any number as an argument, and each time it is called, it prints the square of that number on the screen (that is, the value to the second power).
+# Declare a function called square, that takes any number as an perameter, and each time it is called, it prints the square of that number on the screen (that is, the value to the second power).
 
 # The name of the argument that this function must take is number. Create this variable and assign it any number.
 
-# You should only define the function and create the variable, you should not call the function afterwards.
+#invoke it
+number = 5 
+def square(number):
+
+    print(number ** 2)
+
+print(square(number))
---
+++ main.py
@@ -0,0 +1,53 @@
+# # #function = a block of reusable code
+# # # place () after the function name to invoke it
+
+
+# # def happy_birthday(name, age):
+# #     print("Happy Birthday to {name}!")
+# # print("You are {age} years old!")
+# # print("Happy Birthday to you!")
+# # print()
+
+
+
+# # happy_birthday("Bro", 20)
+# # happy_birthday("Steve", 30)
+# # happy_birthday("Joe", 40)
+
+# # def display_invoice (username, amount, due_date):
+# #     print(f"Hello{username}")
+# #     print(f"Your bill of ${amount:.2f} is due: { due_date}")
+# # display_invoice("JoeSchmo", 100.50, "01/01")
+
+# # #return = statement used to end a function
+# # #          and send a result back to the caller
+
+# # def add (x,y):
+# #     z= x+y 
+# #     return z
+
+# # def subtract (x,y):
+# #     z = x-y
+# #     return z
+
+# # def multiply (x,y):
+# #     z = x * y
+# #     return z
+
+# # def divide (x,y):
+# #     z= x/y
+# #     return z
+
+# # print(add(1,2))
+# # print(subtract(1,2))
+# # print(multiply(1,2))
+# # print(divide(1,2))
+
+# def create_name(fuirst, last):
+#     first = first.captialize()
+#     last = last.captitalize( )
+#     return first + "" + last
+
+# full_name = create_name("spongebob", "squarepants")
+
+# print(full_name)
