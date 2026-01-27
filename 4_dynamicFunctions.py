e3b8c9ab30adb8112af27d4ec99bb1326be70737
asanchez365365 <saintxotic123@gmail.com>
2026-01-27 15:26:32 +0000

    dynamicFunctions

--- 4_dynamicFunctions.py
+++ 4_dynamicFunctions.py
@@ -3,7 +3,11 @@
 
 # Don't call the function, you just need to define it.
 
-
+def all_positives(list):
+      for num in list:
+        if num <= 0:
+            return False
+        return True
 
 
 
@@ -12,9 +16,21 @@
 # Create a function (sum_less) that adds the numbers of a list as long as they are greater than 0 and less than 1000, and returns the result of said sum. Create a numbers variable, storing a list of numbers so we can test it.
 
 
-
+def sum_less(list):
+    total = 0
+    for num in list:
+        if 0 < num < 1000:
+            total += num
+    return total
 
 
 
 # Dynamic Functions Practice #3
 # Create a function (count_even) that counts the number of even numbers that exist in a list (numbers), and returns the result of said count.
+
+def count_even (numbers):
+    return sum (1 for x in numbers if x % 2 == 0)
+
+number= [1,2,3,4,5,6]
+
+count_even(numbers)
